7722dc100251a003b032663e3539a68648e8be49
r01and3 <dimka.pronckin@gmail.com>
2021-03-16 21:33:45 +0300

    Fix comment

--- modules/complex-number/test/test_pronkin_dmitry_complex_number.cpp
+++ modules/complex-number/test/test_pronkin_dmitry_complex_number.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 Pronkin Dmitry
 
 #include <gtest/gtest.h>
-
 #include <tuple>
 #include <memory>
+
 #include "include/complex_number.h"
 
 TEST(Pronkin_Dmitry_ComplexNumberTest, Can_Create_Pointer) {
@@ -15,29 +15,26 @@ TEST(Pronkin_Dmitry_ComplexNumberTest, Can_Create_Pointer) {
     ASSERT_EQ(im, z->getIm());
 }
 
-typedef testing::TestWithParam<std::tuple<double, double>>
-        Pronkin_Dmitry_ComplexNumberTest_Parametrized_2;
-TEST_P(Pronkin_Dmitry_ComplexNumberTest_Parametrized_2,
-       Multiplication_Complex_Conjugate) {
-    double re = std::get<0>(GetParam());
-    double im = std::get<1>(GetParam());
-    ComplexNumber z1(re, im);
-    ComplexNumber z2(re, -im);
-
-    ComplexNumber z = z1 * z2;
-    ComplexNumber control(re * re + im * im, 0.0);
-
-    ASSERT_EQ(control, z);
-}
-
-INSTANTIATE_TEST_CASE_P(/**/, Pronkin_Dmitry_ComplexNumberTest_Parametrized_2,
-    testing::Combine(
-  testing::Values(-5.0, 3.0, 2.0, -3.0),
-  testing::Values(1.3, 7.5, 4.8, 8.0)
-));
-
 typedef testing::TestWithParam<std::tuple<double, double, double, double>>
         Pronkin_Dmitry_ComplexNumberTest_Parametrized_4;
+TEST_P(Pronkin_Dmitry_ComplexNumberTest_Parametrized_4,
+       Multiplication_Complex_Conjugate) {
+    double re1 = std::get<0>(GetParam());
+    double im1 = std::get<1>(GetParam());
+    double re2 = std::get<2>(GetParam());
+    double im2 = std::get<3>(GetParam());
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re1, -im1);
+    ComplexNumber z4(re2, -im2);
+    
+    ComplexNumber control = z1 / z2;
+    ComplexNumber result = z3 / z4;
+
+    ASSERT_DOUBLE_EQ(control.getRe(), result.getRe());
+    ASSERT_DOUBLE_EQ(-control.getIm(), result.getIm());
+}
+
 TEST_P(Pronkin_Dmitry_ComplexNumberTest_Parametrized_4,
        Multiplication_After_Division_Give_Original) {
     double re1 = std::get<0>(GetParam());
@@ -55,9 +52,9 @@ TEST_P(Pronkin_Dmitry_ComplexNumberTest_Parametrized_4,
 }
 
 INSTANTIATE_TEST_CASE_P(/**/, Pronkin_Dmitry_ComplexNumberTest_Parametrized_4,
-    testing::Combine(
-  testing::Values(5.0, 3.0, 5.0, 3.0),
-  testing::Values(1.0, 7.0, 4.0, 8.0),
-  testing::Values(2.0, 6.0, 7.0, 1.0),
-  testing::Values(3.0, 3.0, 3.0, 3.0)
+    testing::Values(
+  std::make_tuple(-3.0, 1.4, 5.7, -13.0),
+  std::make_tuple(11.13, 11.22, 22.63, -1.0),
+  std::make_tuple(2.3, 3.4, 5.6, 7.8),
+  std::make_tuple(3.31, 5.07, -5.0, 3.0)
 ));
